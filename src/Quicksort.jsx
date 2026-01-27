1c031483235b09427ea1bbcc9478d5562b209833
padre_paulas <pounowyou@gmail.com>
2026-01-27 22:36:17 +0200

    remove default readme

--- src/Quicksort.jsx
+++ src/Quicksort.jsx
@@ -1,6 +1,29 @@
-const Quicksort = () => {
-  return <div className="text-sky-400 bg-sky-200 flex m-auto items-center text-center justify-center h-screen w-screen">
-    <h1 className="flex items-center text-center justify-center">Quicksort</h1>
+const Quicksort = (props) => {
+  const { numberOfElements, timePerOperation, value } = props;
+
+  return <div className="flex justify-center h-screen w-screen">
+    <div className="bg-orange-50 ml-0 w-screen">
+      <h1 className="flex items-center text-center justify-center text-4xl mt-4">Quicksort</h1>
+      
+    </div>
+    <div className="bg-orange-400 w-60 text-white grid justify-center text-center">
+      <h2 className="mt-4">Info</h2>
+      <div>
+        <p>Number of elements: {numberOfElements}</p>
+        <div className="h-1 w-35 bg-white mx-auto mt-6 rounded-full"></div>
+      </div>
+      <div className="mb-40">
+        <p>Time per operation, ms: {timePerOperation}</p>
+        <input className="h-1 w-35
+        bg-transparent
+      [&::-webkit-slider-runnable-track]:bg-white
+      [&::-webkit-slider-runnable-track]:h-1
+        [&::-webkit-slider-runnable-track]:rounded-full
+        " min="0" max="1000" value={value} type="range"
+        style={{ accentColor: 'white',  }}
+        ></input>
+      </div>
+    </div>
   </div>
 }
 
