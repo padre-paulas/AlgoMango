197c29b524fe0d2bbd4d72e123fc326970a0bd77
padre_paulas <pounowyou@gmail.com>
2026-02-07 22:44:09 +0200

    bug fix, toggle wasn't prompting the rerender on click

--- src/shared-global/SortedUnsortedToggle.jsx
+++ src/shared-global/SortedUnsortedToggle.jsx
@@ -4,12 +4,11 @@ import setColor from "./set-array-bars-color";
 
 const SortedUnsortedToggle = ({ sorted, setSorted, numberOfElements, setArray, barRefs }) => {
   return <button className={`h-7 w-16 bg-white rounded-full flex justify-center items-center text-center bg-center m-auto my-4 shadow-[inset_0_0_2px_var(--color-midnight-violet)]`} value={sorted}
-  onClick={async () => {
-    const nextSorted = !sorted;
+  onClick={() => {
+    let nextSorted = !sorted;
     setSorted(nextSorted);
     setColor(barRefs, numberOfElements, 'var(--color-amber-glow)')
-    await setArray(sorted ? generateArraySorted(numberOfElements) : generateArray(numberOfElements));
-    console.log("working")
+    setArray((nextSorted ? generateArraySorted(numberOfElements) : generateArray(numberOfElements)));
   }}
   >
     <div className={`border-6 border-midnight-violet w-7 h-7 rounded-full transform transition-all
