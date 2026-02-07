85341545475876b3a9f2edeeca348262eef461f7
padre_paulas <pounowyou@gmail.com>
2026-02-07 15:28:14 +0200

    create basic binary search function

--- src/search-algorithms/BinarySearch/binary-search-func.js
+++ src/search-algorithms/BinarySearch/binary-search-func.js
@@ -1,5 +1,26 @@
-const binarySearchFunc = () => {
-  return;
+const binarySearchFunc = (array, target) => {
+  console.log("working")
+  console.log(array);
+  
+  let left = 0;
+  let right = array.length - 1;
+  let pivot, pivotIndex;
+  while (left < right) {
+    pivotIndex = Math.floor((left + right) / 2);
+    pivot = array[pivotIndex];
+
+    if (pivot === target) {
+      console.log(pivot + " " + pivotIndex)
+      return pivotIndex; 
+    }
+    if (pivot > target) {
+      right = pivotIndex - 1;
+    } else {
+      left = pivotIndex + 1;
+    }
+  }
+  console.log("unsuccessful")
+  return -1;
 }
 
 export default binarySearchFunc;
