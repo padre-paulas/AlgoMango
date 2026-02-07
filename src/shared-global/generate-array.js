d807f33532d37be146e306b75d673f21a0a15bbf
padre_paulas <pounowyou@gmail.com>
2026-02-07 23:58:13 +0200

    Add a note

--- src/shared-global/generate-array.js
+++ src/shared-global/generate-array.js
@@ -5,3 +5,11 @@ const generateArray = (numberOfElements) => {
 }
 
 export default generateArray;
+
+/** 
+* @note
+* Think about doing "random" array generation differently.
+* Generate a sorted array first, then shuffle the elements.
+* This way you will always know you can search elements values
+* by IDs.
+*/
