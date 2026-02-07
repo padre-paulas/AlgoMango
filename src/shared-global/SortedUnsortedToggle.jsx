5717559c6343e40fa4b4a1875cbd36038236d51f
padre_paulas <pounowyou@gmail.com>
2026-02-07 19:55:40 +0200

    create sorted/unsorted toggle

---
+++ src/shared-global/SortedUnsortedToggle.jsx
@@ -0,0 +1,22 @@
+import generateArray from "./generate-array";
+import generateArraySorted from "./generate-array-sorted";
+import setColor from "./set-array-bars-color";
+
+const SortedUnsortedToggle = ({ sorted, setSorted, numberOfElements, setArray, barRefs }) => {
+  return <button className={`h-7 w-16 bg-white rounded-full flex justify-center items-center text-center bg-center m-auto my-4 shadow-[inset_0_0_2px_var(--color-midnight-violet)]`} value={sorted}
+  onClick={async () => {
+    const nextSorted = !sorted;
+    setSorted(nextSorted);
+    setColor(barRefs, numberOfElements, 'var(--color-amber-glow)')
+    await setArray(sorted ? generateArraySorted(numberOfElements) : generateArray(numberOfElements));
+    console.log("working")
+  }}
+  >
+    <div className={`border-6 border-midnight-violet w-7 h-7 rounded-full transform transition-all
+    ease-in-out ${sorted ? '-translate-x-4.5' : 'translate-x-4.5'}
+    `}
+    ></div>
+  </button>
+}
+
+export default SortedUnsortedToggle;
