42277988f294a8e61eee6f206a1aec4d59aaa3a0
padre_paulas <pounowyou@gmail.com>
2026-02-08 16:35:52 +0200

    create LeftSidebar component

---
+++ src/left-sidebar/left-sidebar.jsx
@@ -0,0 +1,14 @@
+const LeftSidebar = () => {
+  return <div className="grid justify-center text-center m-2
+    bg-amber-glow w-60 text-midnight-violet 
+    rounded-xl shadow-[0_0_8px_var(--color-midnight-violet)] border-2 border-midnight-violet/30 
+    ">
+      <h2 className="mt-4 text-2xl">Info</h2>
+      <div>
+      </div>
+      <div className="mb-40">
+      </div>
+    </div>
+}
+
+export default LeftSidebar;
