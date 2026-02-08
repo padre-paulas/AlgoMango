8048b480ad25e08f315e8d5e1164daa29c9a8c68
padre_paulas <pounowyou@gmail.com>
2026-02-08 15:50:41 +0200

    switch to 0-indexed numbering

--- src/shared-global/SliderTarget.jsx
+++ src/shared-global/SliderTarget.jsx
@@ -1,6 +1,6 @@
 const SliderTarget = ({ value, onChange, numberOfElements }) => {
   return <input className="h-1 w-35 bg-transparent" 
-    min={1} max={numberOfElements} value={value} type="range"
+    min={0} max={numberOfElements - 1} value={value} type="range"
     style={{ accentColor: 'var(--color-midnight-violet)' }}
     onChange={(e) => {
       onChange(Number(e.target.value));
