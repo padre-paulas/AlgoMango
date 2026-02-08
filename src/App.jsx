c9c4f26abaa76d0d16a364b40ca941482c573f35
padre_paulas <pounowyou@gmail.com>
2026-02-08 18:23:52 +0200

    create page switch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
 import './App.css'
+import { useState } from 'react'
+import LeftSidebar from './left-sidebar/left-sidebar'
 import Quicksort from './sorting-algorithms/Quicksort/Quicksort'
 import Bubblesort from './sorting-algorithms/Bubblesort/Bubblesort'
 import BinarySearch from './search-algorithms/BinarySearch/BinarySearch'
 import SimpleSearch from './search-algorithms/SimpleSearch/SimpleSearch'
 
 function App() {
+  const [ currentPage, setCurrentPage ] = useState("Quicksort")
   return (
     <>
-      {/* <Quicksort/> */}
-      {/* <Bubblesort /> */}
-      <BinarySearch />
-      {/* <SimpleSearch /> */}
+      <div className='w-screen h-screen flex flex-row bg-amber-glow/20'>
+        <LeftSidebar setCurrentPage={setCurrentPage}/>
+
+        <SwitchPage currentPage={currentPage} />
+      </div>
     </>
   )
+}
+
+const SwitchPage = ({ currentPage }) => {
+  switch (currentPage) {
+    case "Quicksort":
+      return <Quicksort/>
+    case "Bubblesort":
+      return <Bubblesort/>
+    case "SimpleSearch":
+      return <SimpleSearch/>
+    case "BinarySearch":
+      return <BinarySearch/>
+    default:
+      return <Quicksort/>
+  }
 }
 
 export default App
