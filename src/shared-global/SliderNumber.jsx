a962cf2d3c8e72779c48ea43a791ca7b8dd54798
padre_paulas <pounowyou@gmail.com>
2026-02-08 15:35:38 +0200

    Change target if it is larger than array length

--- src/shared-global/SliderNumber.jsx
+++ src/shared-global/SliderNumber.jsx
@@ -1,11 +1,13 @@
 import setColor from "./set-array-bars-color";
-const SliderNumber = ({ value, onChange, setArray, barRefs, generateArrayFunc }) => {
+const SliderNumber = ({ value, onChange, setArray, barRefs, generateArrayFunc, setTarget, target }) => {
   return <input className="h-1 w-35 bg-transparent" 
     min="1" max="100" value={value} type="range"
     style={{ accentColor: 'var(--color-midnight-violet)' }}
     onChange={(e) => {
-      onChange(e.target.value);
-      setArray(generateArrayFunc(e.target.value));
+      let val = Number(e.target.value);
+      onChange(val);
+      setArray(generateArrayFunc(val));
+      Number(target) > val && setTarget(val);
     }}
     onMouseDown={(e) => {
       setColor(barRefs, e.target.value, 'var(--color-amber-glow)');
