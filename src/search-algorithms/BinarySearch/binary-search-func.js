d55bc1c4006517ada3a7f8880356a2279262f364
padre_paulas <pounowyou@gmail.com>
2026-02-08 15:50:18 +0200

    remove logs, use shared setColor function

--- src/search-algorithms/BinarySearch/binary-search-func.js
+++ src/search-algorithms/BinarySearch/binary-search-func.js
@@ -1,25 +1,19 @@
+import setColor from "../../shared-global/set-array-bars-color";
+
 const binarySearchFunc = async (array, target, barRefs, timePerOperation) => {
-  console.log("working")
-  console.log(array);
-  
   let left = 0;
   let right = array.length - 1;
   let pivotIndex;
   while (left <= right) {
     pivotIndex = Math.floor((left + right) / 2);
-    // pivot = array[pivotIndex];
-    console.log("pivotIndex: " + pivotIndex + " target: " + target);
-    console.log("pivotIndex: " + typeof(pivotIndex) + " target: " + typeof(target));
     
-
-    await colorArrayBars(left, right, barRefs, true);
+    setColor(barRefs, left, right + 1, 'var(--color-muted-teal)')
     await sleep(timePerOperation);
     barRefs.current[pivotIndex].style.background = 'var(--color-vibrant-coral)';
     await sleep(timePerOperation);
-    await colorArrayBars(left, right, barRefs, false);
+    setColor(barRefs, left, right + 1, 'var(--color-amber-glow')
     
     if (pivotIndex === target) {
-      console.log(pivotIndex)
       barRefs.current[pivotIndex].style.background = 'var(--color-midnight-violet)';
       return pivotIndex; 
     }
@@ -30,15 +24,7 @@ const binarySearchFunc = async (array, target, barRefs, timePerOperation) => {
     }
     barRefs.current[pivotIndex].style.background = 'var(--color-amber-glow';
   }
-  console.log("unsuccessful")
   return -1;
-}
-
-const colorArrayBars = async (left, right, barRefs, toTeal) => {
-  let color = toTeal ? 'var(--color-muted-teal)' : 'var(--color-amber-glow';
-  for (let i = left; i <= right; i++) {
-    barRefs.current[i].style.background = color;
-  }
 }
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
