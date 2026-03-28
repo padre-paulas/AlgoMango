3791002da041e9fe0c28fe2c1428eff2f95d3486
padre_paulas <pounowyou@gmail.com>
2026-03-28 18:13:58 +0200

    rewrite simple search func in TS

---
+++ src/search-algorithms/SimpleSearch/simple-search-func-sorted.ts
@@ -0,0 +1,27 @@
+import setColor from "../../shared-global/set-array-bars-color";
+import sleep from "../../shared-global/sleep";
+
+const simpleSearchFuncSorted = async (
+  array: number[], 
+  target: number, 
+  barRefs: React.RefObject<(HTMLDivElement | null)[]>, 
+  timePerOperation: number, 
+  sorted: boolean
+) => {
+  setColor(barRefs, 0, array.length, 'var(--color-amber-glow)')
+  for (let i = 0; i < array.length; i++) {
+    const bar = barRefs.current?.[i];
+    if (bar) {
+      bar.style.background = 'var(--color-vibrant-coral)';
+      await sleep(timePerOperation);
+      if (sorted && i === target || !sorted && array[i]) {
+        bar.style.background = 'var(--color-midnight-violet)';
+        return i;
+      }
+      bar.style.background = 'var(--color-amber-glow)';
+    }
+  }
+  return -1;
+}
+
+export default simpleSearchFuncSorted;
